8e976f5c2157ff2966656c446c9a4bd205f1ab76
MacksVersion <107284806+MacksVersion@users.noreply.github.com>
2022-12-05 20:40:57 -0300

    Add files via upload
    Em uma determinada competição de ginástica artística, vários atletas realizam apresentações para uma banca composta por cinco jurados. Cada jurado é responsável por avaliar um critério em específico, sendo eles o tempo de duração da apresentação, originalidade da coreografia, postura do atleta, dificuldade das acrobacias e sincronismo.
    
    A competição em questão possui a seguinte regra de avaliação:
    
    Cada jurado pode fornecer uma nota de um (1) a dez (10);
    A média é calculada com base nas três notas do meio, desconsiderando a maior e menor nota.
    O organizador da competição contratou a sua equipe para criar uma aplicação capaz de receber o nome e notas dos atletas e realizar o devido cálculo. Você foi o encarregado de criar a solução utilizando a linguagem JavaScript.

---
+++ notas-atletas.js
@@ -0,0 +1,36 @@
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+   
+   function crescente(a, b){
+     return (a - b) //faz com que o array seja ordenado numericamente e de ordem crescente.
+   }
+   
+   atletas.forEach (function (atleta){
+       
+     let notas =  atleta.notas.sort(crescente);
+     let media =  0;
+     notas = notas.slice(1,4);
+     notas.forEach (function (nota){
+         media += nota;
+     })
+     
+     console.log(`Atleta: ${atleta.nome} \n Notas Obtidas: ${atleta.notas.sort()} \n Média Válida: ${media}`);
+       
+   });
+   
